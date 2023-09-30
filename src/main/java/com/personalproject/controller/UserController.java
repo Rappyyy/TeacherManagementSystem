77da1cf06ca2f566549cca2ba3be0fe72de30786
unknown <academy4@talentamp.com>
2023-09-30 18:38:08 +0800

    Added APIs

--- src/main/java/com/personalproject/controller/UserController.java
+++ src/main/java/com/personalproject/controller/UserController.java
@@ -4,9 +4,7 @@ package com.personalproject.controller;
 import com.personalproject.entity.User;
 import com.personalproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -19,14 +17,23 @@ public class UserController {
     }
 
     @PostMapping("/employee")
-    public User saveUser(User user) {
+    public User saveUser(@RequestBody User user) {
         return userRepository.save(user);
     }
 
-    @GetMapping("/employee/id")
-    public User getEmployee(String userId) {
+    @GetMapping("/employee/{id}")
+    public User getUsserById(@PathVariable("id") String userId) {
         return userRepository.getUserById(userId);
     }
 
+    @DeleteMapping("/employee/{id}")
+    public String deleteUserById(@PathVariable("id") String userId) {
+        return userRepository.deleteUserById(userId);
+    }
+
+    @PutMapping("/employee/{id}")
+    public String updateEmployee(@PathVariable("id") String userId, @RequestBody User user) {
+        return userRepository.update(userId, user);
+    }
 
 }
