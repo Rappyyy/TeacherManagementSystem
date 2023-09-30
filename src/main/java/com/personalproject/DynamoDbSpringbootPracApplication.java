4f8aae8586307c55746ae158e73ae6241caeca6e
unknown <academy4@talentamp.com>
2023-09-30 15:34:21 +0800

    first commit

---
+++ src/main/java/com/personalproject/DynamoDbSpringbootPracApplication.java
@@ -0,0 +1,13 @@
+package com.personalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDbSpringbootPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamoDbSpringbootPracApplication.class, args);
+	}
+
+}
